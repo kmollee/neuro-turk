a2af01941ec75f69f5d13351fc99628988aabc8b
Alejandro Pulver <alepulver@gmail.com>
2013-06-20 01:33:20 -0300

    Revert last commit

--- app.py
+++ app.py
@@ -32,8 +32,7 @@ def run_simple_httpd_server(app, ip, port=8080):
 if __name__ == '__main__':
    ip   = os.environ['OPENSHIFT_INTERNAL_IP']
    port = 8080
-   #zapp = imp.load_source('application', 'wsgi/application')
-   zapp = imp.load_source('application', 'wsgi/neuroturk/neuroturk/wsgi.py')
+   zapp = imp.load_source('application', 'wsgi/application')
 
    print('Starting WSGIServer on %s:%d ... ' % (ip, port))
    run_simple_httpd_server(zapp.application, ip, port)
